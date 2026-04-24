7b2561b383b40067edab1fd2b454ce0ed461e19f
venom-am <202352302@iiitvadodara.ac.in>
2026-04-24 14:28:10 +0530

    base url render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: "https://chatapp-3-l8w6.onrender.com/api"
 });
 
 axiosInstance.interceptors.request.use((config) => {
